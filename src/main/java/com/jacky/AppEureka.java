dc4460434205129bbe940dd49b8855383c50033a
jacky <tigerzhang@quanyibao.com>
2020-03-23 18:43:36 +0800

    ss

---
+++ src/main/java/com/jacky/AppEureka.java
@@ -0,0 +1,11 @@
+package com.jacky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEureka {
+    public static void main(String[] args) {
+        SpringApplication.run(AppEureka.class, args);
+    }
+}
